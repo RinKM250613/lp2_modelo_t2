f2d2b4718d091a2c6e8f18023d2d8d7ad462b43e
RinKM250613 <I202407573@cibertec.edu.pe>
2025-06-18 20:41:17 -0500

    Proyecto iniciado

---
+++ rebecaACT2/src/main/java/com/modelot2/RebecaAct2Application.java
@@ -0,0 +1,13 @@
+package com.modelot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebecaAct2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebecaAct2Application.class, args);
+	}
+
+}
---
+++ rebecaACT2/src/test/java/com/modelot2/RebecaAct2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebecaAct2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
