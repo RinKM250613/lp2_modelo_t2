0c7c0818addfd93c45421770ff3441abf24b20ec
RinKM250613 <I202407573@cibertec.edu.pe>
2025-06-20 22:00:22 -0500

    little closer

--- rebecaACT2/src/main/java/com/modelot2/repositories/ICategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.modelot2.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.modelot2.models.Categoria;
-
-public interface ICategoriaRepository extends JpaRepository<Categoria, Integer>{
-
-}
